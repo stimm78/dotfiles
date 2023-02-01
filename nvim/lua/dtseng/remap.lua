091794f71390a398c6769989e99e843390868ef8
Deven Tseng <dtseng@ucsb.edu>
2023-02-01 13:01:01 -0800

    Add E and B remaps

--- nvim/lua/dtseng/remap.lua
+++ nvim/lua/dtseng/remap.lua
@@ -61,5 +61,8 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 -- find and replace
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+-- front and end line jumps
+vim.keymap.set("n", "B", "^")
+vim.keymap.set("n", "E", "$")
 -- make executable
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
